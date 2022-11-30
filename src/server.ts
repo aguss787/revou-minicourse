b5c9bf740f035b1b110e8f54bae92b7577528e03
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2022-11-30 12:46:29 +0700

    add POST

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import * as express from 'express';
 
 const app = express.default();
 
+app.post('/', async (req, resp) => {
+    resp.send("Hello World from POST!");
+});
+
 app.get('/', async (req, resp) => {
-    resp.send("Hello World!");
+    resp.send("Hello World from GET!");
 });
 
 app.listen(8000, () => {
