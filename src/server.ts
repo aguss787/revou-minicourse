4e154f278349b5bff966f8b87b4e6e7f9cd45a67
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2022-11-30 12:44:43 +0700

    basic web server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+import * as express from 'express';
+
+const app = express.default();
+
+app.get('/', async (req, resp) => {
+    resp.send("Hello World!");
+});
+
+app.listen(8000, () => {
+    console.log('server listening in port 8000');
+})
