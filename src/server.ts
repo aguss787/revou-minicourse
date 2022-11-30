3781c4ac1d28512598fdf2b79a91b46cc7eae79d
Agus Sentosa Hermawan <agus.hermawan@glints.com>
2022-11-30 12:49:10 +0700

    accept post body

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
+import bodyParser from 'body-parser';
 import * as express from 'express';
 
 const app = express.default();
 
+app.use(
+    bodyParser.urlencoded({ extended: true })
+);
+
 app.post('/', async (req, resp) => {
-    resp.send("Hello World from POST!");
+    resp.send("Hello " + req.body["name"] + " (" + req.body["age"] + ")");
 });
 
 app.get('/', async (req, resp) => {
@@ -12,4 +17,4 @@ app.get('/', async (req, resp) => {
 
 app.listen(8000, () => {
     console.log('server listening in port 8000');
-})
+});
